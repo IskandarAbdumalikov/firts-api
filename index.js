a44ecc211f26719205bf1b2d5f68d1e1c319d482
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-30 21:10:38 +0500

    added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
+
+
 const Users = require("./routes/users");
 const Products = require("./routes/products");
+
+
 app.use("/products", Products);
 app.use("/users", Users);
 
--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const DATA = {
       fname: "John",
       username: "John32",
       password: "12345",
+      image:
+        "https://cdn.britannica.com/41/218341-050-51D8903F/American-actor-John-Krasinski-2020.jpg",
+    },
+    {
+      id: 2,
+      fname: "Mike",
+      username: "Mike32",
+      password: "11111",
+      image:
+        "https://cdn.britannica.com/01/157001-050-282F9606/John-Lithgow-2011.jpg",
     },
   ],
   products: [
@@ -13,7 +23,21 @@ const DATA = {
       title: "samsung s20",
       category: "phone",
       price: 12345,
-      url: "https://picsum.photos/200/300",
+      url: "https://univershop.uz/wp-content/uploads/2021/12/780-1.jpg",
+    },
+    {
+      id: 2,
+      title: "Iphone 15",
+      category: "phone",
+      price: 12345,
+      url: "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+    },
+    {
+      id: 3,
+      title: "Samsung s22 ultra",
+      category: "phone",
+      price: 12345,
+      url: "https://images.samsung.com/is/image/samsung/p6pim/uz_uz/2202/gallery/uz-uz-galaxy-s22-ultra-s908-sm-s908bdrdskz-530860882?$650_519_PNG$",
     },
   ],
 };
